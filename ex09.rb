ae159424cf3aa5328f7c48342487f0c825d3fb07
Jason Ganub <jasonganub@gmail.com>
2017-11-12 22:52:47 -0800

    More printing and multiline printing.

--- ex09.rb
+++ ex09.rb
@@ -1,15 +1,12 @@
-tabby_cat = "\tI'm tabbed in."
-persian_cat = "I'm split\non a line."
-backslash_cat = "I'm \\ a \\ cat."
+days = "Mon Tue Wed Thu Fri Sat Sun"
+months = "Jan\nFeb\nMar\nApr\nMay\nJun\nJul\nAug"
 
-fat_cat = """
-I'll do a list:
-\t* Cat food
-\t* Fishies
-\t* Catnip\n\t* Grass
-"""
+puts "Here are the days: #{days}"
+puts "Here are the months: #{months}"
 
-puts tabby_cat
-puts persian_cat
-puts backslash_cat
-puts fat_cat
+puts %q{
+There's something going on here.
+With this weird quote
+We'll be able to type as much as we like.
+Even 4 lines if we want, or 5, or 6.
+}
