09049396cb9d12eec775b4bc7bba7d7b1ee67d95
Jason Ganub <jasonganub@gmail.com>
2017-12-05 15:33:45 -0800

    Retrieving values from arrays based on ordinal and cardinal numbers.

---
+++ ex34.rb
@@ -0,0 +1,10 @@
+animals = ['bear', 'ruby', 'peacock', 'kangaroo', 'whale', 'platypus']
+
+puts "The animal at 1 is the 2nd animal and is a #{animals[1]}"
+puts "The third animal is at 2 and is a #{animals[2]}"
+puts "The first animal is at 0 and is a #{animals[0]}"
+puts "The animal at 3 is the 4th animal and is a #{animals[3]}"
+puts "The fifth animal is at 4 and is a #{animals[4]}"
+puts "The animal at 2 is the 3rd animal and is a #{animals[2]}"
+puts "The sixth animal is at 5 and is a #{animals[5]}"
+puts "The animal at 4 is at 3 and is a #{animals[4]}"
